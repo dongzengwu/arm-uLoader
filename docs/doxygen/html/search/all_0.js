fbc636b66b6c96e3cf6a94d92904f3121ca82a04
Alexander Voigt <Voigt-Alexander@linux.com>
2014-11-16 20:01:50 +0100

    fix doxygen ignore pattern

--- docs/doxygen/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['muh',['muh',['../test_8c.html#a057a3cfb45b6ff773b85e7f9fa5b8e81',1,'muh(int count):&#160;test.c'],['../test_8h.html#a057a3cfb45b6ff773b85e7f9fa5b8e81',1,'muh(int count):&#160;test.c']]]
-];
